1dd7b713ac7d129d933da6637891b0d11d96bce1
Piotr Kowalski <piotr@pkowalski.pw>
2021-11-21 19:14:25 +0100

    Add quick link creation

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Controllers/LinksController.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Controllers/LinksController.cs
@@ -31,11 +31,27 @@ namespace Rinkudesu.Gateways.Webui.Controllers
             return View(_mapper.Map<List<LinkDto>>(links.ToList()));
         }
 
+        [HttpGet]
+        public IActionResult Create() => View();
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind] LinkDto newLink)
         {
             if (!ModelState.IsValid) return BadRequest(); //TODO: make this prettier
+
+            var isSuccess = await _client.CreateLink(newLink);
+            if (!isSuccess) return BadRequest(); //TODO: some display for error would be nice here
+            return Redirect(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> QuickCreate(string? url)
+        {
+            if (string.IsNullOrEmpty(url)) return BadRequest(); //TODO: make this prettier
+
+            var newLink = new LinkDto { Title = url, LinkUrl = url, PrivacyOptions = LinkPrivacyOptions.Private };
 
             var isSuccess = await _client.CreateLink(newLink);
             if (!isSuccess) return BadRequest(); //TODO: some display for error would be nice here
