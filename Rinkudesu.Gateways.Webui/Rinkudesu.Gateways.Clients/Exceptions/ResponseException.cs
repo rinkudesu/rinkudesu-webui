e414b12d502ff98a4f5a942956ea2fe6cabbf467
Piotr Kowalski <piotr@pkowalski.pw>
2022-01-07 01:35:18 +0100

    Add new exception types

---
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/Exceptions/ResponseException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace Rinkudesu.Gateways.Clients.Exceptions;
+
+[Serializable]
+[ExcludeFromCodeCoverage]
+public class ResponseException : Exception
+{
+    public ResponseException()
+    {
+    }
+
+    public ResponseException(string message) : base(message)
+    {
+    }
+
+    public ResponseException(string message, Exception inner) : base(message, inner)
+    {
+    }
+
+    protected ResponseException(
+        SerializationInfo info,
+        StreamingContext context) : base(info, context)
+    {
+    }
+}
