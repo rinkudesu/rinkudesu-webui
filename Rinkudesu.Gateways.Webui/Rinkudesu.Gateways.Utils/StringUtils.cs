863b451d8ca7a91d3ae616e9e47811b76a1d92f3
Piotr Kowalski <piotr@pkowalski.pw>
2022-01-08 23:49:48 +0100

    Add base64 encoder

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/StringUtils.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/StringUtils.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace Rinkudesu.Gateways.Utils;
 
@@ -7,4 +8,9 @@ public static class StringUtils
     [ExcludeFromCodeCoverage]
     [SuppressMessage("Design", "CA1054", MessageId = "URI-like parameters should not be strings")]
     public static Uri ToUri(this string uriString) => new Uri(uriString, UriKind.RelativeOrAbsolute);
+
+    public static string ToBase64(this string text)
+    {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+    }
 }
