3177453bdb1d5a4a72582c2f28e49fb81f051978
Piotr Kowalski <piotr@pkowalski.pw>
2022-01-08 23:57:32 +0100

    Remove base64 encoder

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/StringUtils.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/StringUtils.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 namespace Rinkudesu.Gateways.Utils;
 
@@ -8,9 +7,4 @@ public static class StringUtils
     [ExcludeFromCodeCoverage]
     [SuppressMessage("Design", "CA1054", MessageId = "URI-like parameters should not be strings")]
     public static Uri ToUri(this string uriString) => new Uri(uriString, UriKind.RelativeOrAbsolute);
-
-    public static string ToBase64(this string text)
-    {
-        return Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
-    }
 }
