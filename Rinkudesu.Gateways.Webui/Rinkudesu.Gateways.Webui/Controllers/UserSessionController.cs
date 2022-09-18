c9506250646a52fe853c79c7c6562544a435b245
Piotr Kowalski <piotr@pkowalski.net>
2022-09-18 11:05:11 +0200

    Add a button to post delete user message

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Controllers/UserSessionController.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Controllers/UserSessionController.cs
@@ -3,13 +3,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rinkudesu.Gateways.MessageQueues;
+using Rinkudesu.Gateways.Utils;
 using Rinkudesu.Gateways.Webui.Models;
 using Rinkudesu.Gateways.Webui.Utils;
+using Rinkudesu.Kafka.Dotnet.Base;
 
 namespace Rinkudesu.Gateways.Webui.Controllers
 {
     public class UserSessionController : Controller
     {
+        private readonly IKafkaProducer _kafkaProducer;
+
+        public UserSessionController(IKafkaProducer kafkaProducer)
+        {
+            _kafkaProducer = kafkaProducer;
+        }
+
 // Disable antiforgery token check
 #pragma warning disable CA5391
         [HttpGet]
@@ -28,6 +38,16 @@ namespace Rinkudesu.Gateways.Webui.Controllers
             if (!User.Identity!.IsAuthenticated) return Redirect("/");
             await HttpContext.SignOutAsync();
             return Redirect($"{KeycloakSettings.Current.Authority}/protocol/openid-connect/logout?redirect_uri={HttpContext.GetEncodedBasePath()}");
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser()
+        {
+            //todo: this is just a mock, it just posts "delete user" message to queue, but doesn't actually delete any users
+            await _kafkaProducer.ProduceUserDeleted(User.GetIdAsGuid());
+            return LocalRedirect("/");
         }
     }
 }
