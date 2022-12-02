4c318f68e35882e55fd44efa715fd7db69f7d3c4
Piotr Kowalski <piotr@pkowalski.net>
2022-12-02 19:54:02 +0100

    Add return url validation with tests

---
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Middleware/ReturnUrlValidationMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Rinkudesu.Gateways.Webui.Middleware;
+
+public class ReturnUrlValidationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ReturnUrlValidationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    private const string RETURN_URL_NAME = "returnUrl";
+    private readonly Regex _returnUrlRegex = new("[rR]eturn[uU]rl[^=]*=[^?]*[&$]");
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Query.TryGetValue(RETURN_URL_NAME, out var returnUrl))
+        {
+            var sanitised = SanitiseUrl(returnUrl);
+            var newQueryString = _returnUrlRegex.Replace(context.Request.QueryString.Value!, $"returnUrl={sanitised}&");
+            context.Request.QueryString = new QueryString(newQueryString);
+        }
+        if (context.Request.HasFormContentType && context.Request.Form.TryGetValue(RETURN_URL_NAME, out returnUrl))
+        {
+            var sanitised = SanitiseUrl(returnUrl);
+            var formDictionary = context.Request.Form.Where(f => f.Key != RETURN_URL_NAME).ToDictionary(k => k.Key, v => v.Value);
+            formDictionary[RETURN_URL_NAME] = sanitised;
+            context.Request.Form = new FormCollection(formDictionary, context.Request.Form.Files);
+        }
+        await _next(context);
+    }
+
+    private static string SanitiseUrl(string originalUrl)
+    {
+        if (!Uri.TryCreate(originalUrl, UriKind.RelativeOrAbsolute, out var returnUri))
+            return originalUrl; //let it be if it doesn't parse so that we don't replace valid url elements by mistake
+
+        if (returnUri.IsAbsoluteUri)
+            return returnUri.PathAndQuery;
+
+        return originalUrl;
+    }
+}
