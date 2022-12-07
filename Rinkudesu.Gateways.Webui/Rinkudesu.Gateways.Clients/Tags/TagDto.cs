f9e0081c93606abc8c2c1e1c107c49b479c1bf4d
Piotr Kowalski <piotr@pkowalski.net>
2022-12-07 21:22:55 +0100

    Add `TagDto`

---
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/Tags/TagDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rinkudesu.Gateways.Clients.Tags;
+
+public class TagDto
+{
+    public Guid Id { get; set; }
+
+    [Display(Name = nameof(Resources.Tags.TagDto.name), ResourceType = typeof(Resources.Tags.TagDto))]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+}
