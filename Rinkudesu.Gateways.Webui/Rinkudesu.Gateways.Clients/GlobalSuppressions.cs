ba93dc9d8ac73f9855b7d5cf9524e1d93c6c79ca
Piotr Kowalski <piotr@pkowalski.net>
2022-12-09 20:27:31 +0100

    Disable warning

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/GlobalSuppressions.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/GlobalSuppressions.cs
@@ -4,4 +4,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Usage", "CA2254:Template should be a static expression", Scope = "module")]
 [assembly: SuppressMessage("Naming", "CA1727:Use PascalCase for named placeholders", Scope = "module")]
 [assembly: SuppressMessage("Performance", "CA1848", Scope = "module")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Scope = "module")]
 [assembly: CLSCompliant(false)]
