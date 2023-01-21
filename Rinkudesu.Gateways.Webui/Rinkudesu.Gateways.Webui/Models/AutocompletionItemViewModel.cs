dab30e717eec4c1a5957fa09493c45d6a472c583
Piotr Kowalski <piotr@pkowalski.net>
2023-01-21 19:12:19 +0100

    Initialise properties to avoid nrt warnings

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Models/AutocompletionItemViewModel.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Models/AutocompletionItemViewModel.cs
@@ -4,10 +4,11 @@ using System.Text.Json.Serialization;
 namespace Rinkudesu.Gateways.Webui.Models;
 
 [ExcludeFromCodeCoverage]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
 public class AutocompletionItemViewModel
 {
     [JsonPropertyName("id")]
-    public string ItemId { get; set; }
+    public string ItemId { get; init; } = string.Empty;
     [JsonPropertyName("data")]
-    public string ItemData { get; set; }
+    public string ItemData { get; init; } = string.Empty;
 }
