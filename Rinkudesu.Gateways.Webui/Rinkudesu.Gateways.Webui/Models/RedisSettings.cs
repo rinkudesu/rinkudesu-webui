647c76140751deed4284c680a8c695d275d022bb
Piotr Kowalski <piotr@pkowalski.net>
2023-04-29 22:05:18 +0200

    Add session ticket storage in redis

---
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Models/RedisSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
+namespace Rinkudesu.Gateways.Webui.Models;
+
+[ExcludeFromCodeCoverage]
+public class RedisSettings
+{
+    private static RedisSettings? current;
+
+    public static RedisSettings Current
+    {
+        get => current ?? throw new InvalidOperationException("Redis settings have not been initialised");
+        set
+        {
+            if (current is not null)
+                throw new InvalidOperationException("Redis settings have already been initialised");
+
+            current = value;
+        }
+    }
+
+    public string Address { get; }
+
+    public RedisSettings()
+    {
+        Address = Environment.GetEnvironmentVariable("RINKUDESU_REDIS_ADDRESS") ?? throw new InvalidOperationException("RINKUDESU_REDIS_ADDRESS env variable must be set");
+    }
+
+    [SuppressMessage("Design", "CA1024:Use properties where appropriate")] // this is most certainly not appropriate
+    public RedisCacheOptions GetRedisOptions() => new RedisCacheOptions { Configuration = Address };
+}
