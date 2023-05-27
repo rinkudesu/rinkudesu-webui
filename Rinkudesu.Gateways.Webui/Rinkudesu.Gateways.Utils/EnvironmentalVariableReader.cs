5064a42f3c2b4f1586d238e6300c16518016c091
Piotr Kowalski <piotr@pkowalski.net>
2023-05-27 19:54:44 +0200

    Exclude class from code coverage

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/EnvironmentalVariableReader.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Utils/EnvironmentalVariableReader.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Rinkudesu.Gateways.Utils;
 
+[ExcludeFromCodeCoverage]
 public static class EnvironmentalVariableReader
 {
     public static bool RegistrationEnabled => string.IsNullOrEmpty(Environment.GetEnvironmentVariable("RINKUDESU_DISABLE_REGISTRATION"));
