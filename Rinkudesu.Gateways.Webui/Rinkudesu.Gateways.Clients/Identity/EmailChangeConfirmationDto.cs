9078eb16c9c203e410a51dfcd656a6fc6a740156
Piotr Kowalski <piotr@pkowalski.net>
2023-05-28 10:41:14 +0200

    Add value initialisation

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/Identity/EmailChangeConfirmationDto.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Clients/Identity/EmailChangeConfirmationDto.cs
@@ -5,6 +5,6 @@ namespace Rinkudesu.Gateways.Clients.Identity;
 public class EmailChangeConfirmationDto
 {
     public Guid UserId { get; set; }
-    public string NewEmail { get; set; }
-    public string Token { get; set; }
+    public string NewEmail { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
 }
