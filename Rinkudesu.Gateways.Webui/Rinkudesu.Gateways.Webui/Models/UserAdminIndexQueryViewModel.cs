0162f035290cfea256ffc3c1325caa10ad400cd1
Piotr Kowalski <piotr@pkowalski.net>
2023-06-11 10:42:11 +0200

    Add localisation to account admin query model

--- Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Models/UserAdminIndexQueryViewModel.cs
+++ Rinkudesu.Gateways.Webui/Rinkudesu.Gateways.Webui/Models/UserAdminIndexQueryViewModel.cs
@@ -9,14 +9,19 @@ namespace Rinkudesu.Gateways.Webui.Models;
 [ExcludeFromCodeCoverage]
 public class UserAdminIndexQueryViewModel
 {
+    [Display(Name = nameof(Resources.Models.Identity.UserAdminIndexQueryViewModel.sortOptions), ResourceType = typeof(Resources.Models.Identity.UserAdminIndexQueryViewModel))]
     public SortOptions SortOption { get; set; }
     [Range(0, int.MaxValue)]
     public int Skip { get; set; }
     [Range(0, int.MaxValue)]
     public int Take { get; set; } = 20;
+    [Display(Name = nameof(Resources.Models.Identity.UserAdminIndexQueryViewModel.emailContains), ResourceType = typeof(Resources.Models.Identity.UserAdminIndexQueryViewModel))]
     public string? EmailContains { get; set; }
+    [Display(Name = nameof(Resources.Models.Identity.UserAdminIndexQueryViewModel.isAdmin), ResourceType = typeof(Resources.Models.Identity.UserAdminIndexQueryViewModel))]
     public bool IsAdmin { get; set; }
+    [Display(Name = nameof(Resources.Models.Identity.UserAdminIndexQueryViewModel.emailConfirmed), ResourceType = typeof(Resources.Models.Identity.UserAdminIndexQueryViewModel))]
     public bool? EmailConfirmed { get; set; }
+    [Display(Name = nameof(Resources.Models.Identity.UserAdminIndexQueryViewModel.lockedOnly), ResourceType = typeof(Resources.Models.Identity.UserAdminIndexQueryViewModel))]
     public bool LockedOnly { get; set; }
 
     public enum SortOptions
